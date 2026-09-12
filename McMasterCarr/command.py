050e267dbd558d07a422512c606d22205ba4ae1b
Daniel Shaw <dan@danieljshaw.com>
2026-09-12 16:11:43 -0400

    Keep catalog window alive after command activation

--- McMasterCarr/command.py
+++ McMasterCarr/command.py
@@ -9,6 +9,8 @@ ERROR_MESSAGE = (
     "PySide6.QtWebEngineCore and PySide6.QtWebEngineWidgets."
 )
 
+_catalog_window = None
+
 
 class OpenCatalogCommand:
     def GetResources(self):
@@ -22,18 +24,22 @@ class OpenCatalogCommand:
         return True
 
     def Activated(self):
+        global _catalog_window
         try:
-            from PySide6 import QtWebEngineCore, QtWebEngineWidgets  # noqa: F401
+            from PySide6 import QtWidgets, QtWebEngineCore, QtWebEngineWidgets  # noqa: F401
+            from .browser import McMasterBrowserWindow
+            if _catalog_window is None:
+                _catalog_window = McMasterBrowserWindow(Gui.getMainWindow())
+            _catalog_window.show()
+            _catalog_window.raise_()
+            _catalog_window.activateWindow()
         except ImportError:
-            Gui.showMainWindow()
-            Gui.activeDocument()
             from PySide6 import QtWidgets
             QtWidgets.QMessageBox.critical(None, "McMaster-Carr Importer", ERROR_MESSAGE)
-            return
-
-        from .browser import McMasterBrowserWindow
-        window = McMasterBrowserWindow(Gui.getMainWindow())
-        window.show()
-        window.raise_()
-        window.activateWindow()
-        self._window = window
+        except Exception as exc:
+            from PySide6 import QtWidgets
+            QtWidgets.QMessageBox.critical(
+                Gui.getMainWindow(),
+                "McMaster-Carr Importer",
+                f"Could not open McMaster-Carr catalog:\n{exc}",
+            )
